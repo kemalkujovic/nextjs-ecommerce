78c72326dae654ddaf4d4706d0bccfe436f86588
kemalkujovic <kemalkujovic1111@gmail.com>
2023-12-07 23:43:12 +0100

    feat delete actions

--- actions/get-products.tsx
+++
@@ -1,9 +0,0 @@
-import { type Product } from "@/types";
-
-const getProducts = async (): Promise<Product[]> => {
-  const res = await fetch("/api/product/");
-
-  return res.json();
-};
-
-export default getProducts;

43128b3340f7f9c794916016bd901408a975e5f3
kemalkujovic <kemalkujovic1111@gmail.com>
2023-12-17 20:11:14 +0100

    style cart empty page

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import useCart from "@/hooks/use-cart";
 import CartItem from "./_components/cart-item";
 import Footer from "@/components/footer";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 const CartPage = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -19,22 +20,25 @@ const CartPage = () => {
   }
 
   return (
-    <div className="bg-white">
+    <div className="bg-white ">
       <div className="mx-auto max-w-7xl min-h-screen">
         <div className="px-4 py-8 sm:px-6 lg:px-8">
           <h1 className="text-3xl font-bold text-black">Shopping Cart</h1>
-          <div className=" lg:grid lg:grid-cols-12 lg:items-start gap-x-12">
+          {cart.items.length === 0 && (
+            <div className="flex flex-col items-center justify-center py-40">
+              <ShoppingCartIcon style={{ fontSize: "10rem" }} />
+              <p className="text-neutral-500">Your cart is empty.</p>
+            </div>
+          )}
+          <div className="lg:grid lg:grid-cols-12 lg:items-start gap-x-12">
             <div className="lg:col-span-7">
-              {cart.items.length === 0 && (
-                <p className="text-neutral-500">No items added to cart.</p>
-              )}
               <ul>
                 {cart.items.map((item, index) => (
                   <CartItem key={index} data={item} />
                 ))}
               </ul>
             </div>
-            {/* <Summary /> */}
+            {/* todo: <Summary /> */}
           </div>
         </div>
       </div>

e7c0852abda92fc282df7ce762f196ab23f46a66
kemalkujovic <kemalkujovic1111@gmail.com>
2023-12-17 19:27:03 +0100

    add logic for cart quantity

--- app/(routes)/cart/_components/cart-item.tsx
+++ app/(routes)/cart/_components/cart-item.tsx
@@ -1,19 +1,29 @@
 import Image from "next/image";
 
-import useCart from "@/hooks/use-cart";
+import useCart, { CartItem } from "@/hooks/use-cart";
 import { Product } from "@/types";
 import CloseIcon from "@mui/icons-material/Close";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
 
 interface CartItemProps {
-  data: Product;
+  data: CartItem;
 }
 
 const CartItem: React.FC<CartItemProps> = ({ data }) => {
   const cart = useCart();
   const baseUrl = "https://kemal-web-storage.s3.eu-north-1.amazonaws.com";
 
+  const onRemoveAll = () => {
+    cart.removeAll(data);
+  };
+
   const onRemove = () => {
     cart.removeItem(data);
+  };
+
+  const onAdd = () => {
+    cart.addItem(data);
   };
 
   return (
@@ -29,7 +39,7 @@ const CartItem: React.FC<CartItemProps> = ({ data }) => {
       <div className="relative ml-4 flex flex-1 flex-col justify-between sm:ml-6">
         <div className="absolute z-10 right-0 top-0">
           <button
-            onClick={onRemove}
+            onClick={onRemoveAll}
             className="rounded-full flex items-center justify-center bg-white border shadow-md p-2 hover:scale-110 transition"
           >
             <CloseIcon />
@@ -46,12 +56,25 @@ const CartItem: React.FC<CartItemProps> = ({ data }) => {
               {data.size}
             </p>
           </div>
-          <div>
+          <div className="flex flex-col mt-2 gap-y-3 max-md:flex-row max-md:justify-between max-md:items-center">
             <p className="text-lg text-gray-900 font-semibold">
-              {data.finalPrice
-                ? "$" + data.finalPrice.toFixed(2)
-                : `${Number(data?.price).toFixed(2)}`}
+              {data.totalPrice
+                ? `$${data.totalPrice.toFixed(2)}`
+                : data.finalPrice
+                ? `$${data.finalPrice.toFixed(2)}`
+                : `$${Number(data?.price).toFixed(2)}`}
             </p>
+            <div className="flex max-md:justify-end w-full">
+              <div className="border w-28 rounded-3xl p-2 gap-2 flex justify-between">
+                <button onClick={onRemove}>
+                  <RemoveIcon />
+                </button>
+                <p>{data.quantity}</p>
+                <button onClick={onAdd}>
+                  <AddIcon />
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- hooks/use-cart.tsx
+++ hooks/use-cart.tsx
@@ -4,11 +4,16 @@ import { persist, createJSONStorage } from "zustand/middleware";
 
 import { Product } from "@/types";
 
+export interface CartItem extends Product {
+  quantity: number;
+  totalPrice?: number;
+}
+
 interface CartStore {
-  items: Product[];
+  items: CartItem[];
   addItem: (data: Product) => void;
   removeItem: (data: Product) => void;
-  removeAll: () => void;
+  removeAll: (data: Product) => void;
 }
 
 const useCart = create(
@@ -17,17 +22,51 @@ const useCart = create(
       items: [],
       addItem: (data: Product) => {
         const currentItems = get().items;
-        const existingItem = currentItems.find(
+
+        const existingItem = currentItems.findIndex(
           (item) => item.id === data.id && item.size === data.size
         );
 
-        if (existingItem) {
-          return toast.error("Item already in cart.");
+        if (existingItem !== -1) {
+          const updatedItems = [...currentItems];
+          updatedItems[existingItem].quantity += 1;
+
+          updatedItems[existingItem].totalPrice =
+            updatedItems[existingItem].quantity *
+            (data.finalPrice && data.finalPrice > 0
+              ? data.finalPrice
+              : +data.price);
+
+          set({ items: updatedItems });
+          toast.success("Item added to cart.");
+        } else {
+          set({ items: [...currentItems, { ...data, quantity: 1 }] });
+          toast.success("Item added to cart.");
         }
-        set({ items: [...get().items, data] });
-        toast.success("Item added to cart.");
       },
       removeItem: (data: Product) => {
+        const currentItems = get().items;
+        const existingItemIndex = currentItems.findIndex(
+          (item) => item.id === data.id && item.size === data.size
+        );
+
+        if (existingItemIndex !== -1) {
+          const updatedItems = [...currentItems];
+          if (updatedItems[existingItemIndex].quantity > 1) {
+            updatedItems[existingItemIndex].quantity -= 1;
+            updatedItems[existingItemIndex].totalPrice =
+              updatedItems[existingItemIndex].quantity *
+              (data.finalPrice && data.finalPrice > 0
+                ? data.finalPrice
+                : +data.price);
+          } else {
+            updatedItems.splice(existingItemIndex, 1);
+          }
+          set({ items: updatedItems });
+          toast.success("Item removed from cart.");
+        }
+      },
+      removeAll: (data: Product) => {
         const currentItems = get().items;
         const remainingItems = currentItems.filter(
           (item) => !(item.id === data.id && item.size === data.size)
@@ -35,7 +74,6 @@ const useCart = create(
         set({ items: remainingItems });
         toast.success("Item removed from cart.");
       },
-      removeAll: () => set({ items: [] }),
     }),
     {
       name: "cart-storage",

f75115bd375acff431ccd20ed6a117d2704a0f66
Kemal Kujovic <kemalkujovic1111@gmail.com>
2024-01-14 20:33:55 +0100

    fix uploadFileToS3 function for deploy

--- app/api/product/route.ts
+++ app/api/product/route.ts
@@ -4,7 +4,7 @@ import { PutObjectCommand } from "@aws-sdk/client-s3";
 import { auth } from "@clerk/nextjs";
 import { s3Client } from "@/lib/s3";
 
-export async function uploadFileToS3(file: any, fileName: any) {
+ async function uploadFileToS3(file: any, fileName: any) {
   const fileBuffer = file;
 
   const randomSuffix = Math.random().toString(36).substring(7);
